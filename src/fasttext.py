b39119693c7da369a52105722c77bebe76cbff08
837477 <8374770@gmail.com>
2021-01-28 17:20:17 +0900

    페스트 텍스트 테스트 학습 코드

---
+++ src/fasttext.py
@@ -0,0 +1,21 @@
+import os
+from gensim.models import Word2Vec, KeyedVectors, FastText
+from db_connection import Mongo
+
+DB = Mongo()
+
+# 데이터 전처리 #################################################
+db_result = list(DB.cursor()['gallery'].find({"pass": 1}))
+result = [post['join'] for post in db_result]
+
+
+#### HyperParameter
+vec_size = 20
+windows = 6
+min_count = 10
+iteration = 100
+workers = 4
+
+model = FastText(sentences=result, size=vec_size, window=windows, min_count=min_count, iter=iteration, workers=workers)
+model_result = model.wv.most_similar("AMD 5800X")
+print(model_result)
