2cb49a9a9c5de785d6b1a864abf8d5eeb6db3302
837477 <8374770@gmail.com>
2021-01-30 22:13:10 +0900

    학습 코드 수정

--- src/comtris.py
+++ src/comtris.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torch.nn as nn
+import numpy as np
 from pymongo import MongoClient
 
 
@@ -30,8 +31,7 @@ class Comtris():
             "POWER": torch.load("./model/POWER"),
         }
         for part in self.model:
-            self.model[part].eval()
-        
+            self.model[part].eval() 
         self.index_dict = {
             "CPU": self.db['master_config'].find_one({"key": "CPU_dict"})['value'],
             "VGA": self.db['master_config'].find_one({"key": "VGA_dict"})['value'],
@@ -40,15 +40,30 @@ class Comtris():
             "SSD": self.db['master_config'].find_one({"key": "SSD_dict"})['value'],
             "POWER": self.db['master_config'].find_one({"key": "POWER_dict"})['value']
         }
+        self.part_needs = self.db['master_config'].find_one({"key": "needs"})['value']
         self.index = {}
         for part in self.index_dict:
             for p_i in self.index_dict[part]["part_to_index"]:
                 self.index_dict[part]["part_to_index"][p_i] = int(self.index_dict[part]["part_to_index"][p_i])
             self.index.update(self.index_dict[part]["part_to_index"])
     
+    def part(self):
+        part = {
+            "CPU": list(self.index_dict['CPU']['part_to_index'].keys()),
+            "VGA": list(self.index_dict['VGA']['part_to_index'].keys()),
+            "M/B": list(self.index_dict['M/B']['part_to_index'].keys()),
+            "RAM": list(self.index_dict['RAM']['part_to_index'].keys()),
+            "SSD": list(self.index_dict['SSD']['part_to_index'].keys()),
+            "POWER": list(self.index_dict['POWER']['part_to_index'].keys())
+        }
+        return part
+    
+    def needs(self):
+        return self.part_needs
+
     def prediction(self, parts, target):
         # 예측 데이터 개수 확인
-        if len(parts) != 5:
+        if len(parts) != len(self.part_needs[target]):
             return False
         
         if target not in {"CPU", "VGA", "M/B", "RAM", "SSD", "POWER"}:
@@ -73,5 +88,34 @@ if __name__ == "__main__":
 
     # 순서 매우 중요!!
     # ["AMD 3100", "ASROCK A320M", "ASROCK RX570", "3200 8G", "500GB", "600W"]
-    result = CT.prediction(["GTX1050TI ASUS", "A320 ASUS", "2400 8GB", "500GB", "600W"], "CPU")
+    # [CPU, M/B, VGA, RAM, SSD, POWER]
+
+    needs = CT.needs()
+    part = CT.part()
+    # CPU TEST
+    '''
+    for i in range(5):
+        x = []
+        for p in part:
+            if p not in needs['CPU']:
+                continue
+            x.append(np.random.choice(part[p]))
+        result = CT.prediction(x, "CPU")
+        print(x)
+        print(result)
+        print("#" * 100)
+    # VGA TEST
+    for i in range(5):
+        x = []
+        for p in part:
+            if p not in needs['VGA']:
+                continue
+            x.append(np.random.choice(part[p]))
+        result = CT.prediction(x, "VGA")
+        print(x)
+        print(result)
+        print("#" * 100)
+    '''
+
+    result = CT.prediction(["GTX1660SUPER ASUS", "A320 ASUS", "3200 16GB", "1TB", "600W"], "CPU")
     print(result)
--- src/learning.py
+++ src/learning.py
@@ -45,7 +45,7 @@ y_column = ["CPU"]
 
 db = MongoClient(os.environ['COMTRIS_MONGODB_URI'])['COMTRIS']
 
-date = datetime.strptime('2020-10-01', '%Y-%m-%d')
+date = datetime.strptime('2020-11-01', '%Y-%m-%d')
 train_data = list(db['gallery'].find({"pass": 1, "shop_date": {'$gt': date}}))\
            + list(db['pc_qoute'].find({"pass": 1, "shop_date": {'$gt': date}}))\
            + list(db['review'].find({"pass": 1, "shop_date": {'$gt': date}}))
@@ -117,7 +117,7 @@ model = Net(input_dim, output_dim)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
-epochs = 100
+epochs = 2000
 for epoch in tqdm(range(epochs)):
     for x, y in trainloader:
         #clear gradient 
@@ -131,6 +131,15 @@ for epoch in tqdm(range(epochs)):
 
     if epoch % 100 == 0:
         print('epoch {}, loss {}'.format(epoch, loss.item()))
+
+# needs 저장
+needs = db['master_config'].find_one({"key": "needs"})
+if not needs:
+    db['master_config'].insert_one({"key": "needs", "value": {}})
+    needs = {}
+if y_column[0] not in needs:
+    needs[y_column[0]] = x_column
+db['master_config'].update_one({"key": "needs"}, {"$set": {"value": needs}})
 
 # 모델 저장
 if y_column[0] == "M/B":
